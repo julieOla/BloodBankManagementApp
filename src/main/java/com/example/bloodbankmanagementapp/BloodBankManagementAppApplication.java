208403cc2f47607b41620e4e4271db80fff05043
JulieOlamijuwon <d00215779@student.dkit.ie>
2025-01-30 17:14:26 +0000

    SetUp commit

---
+++ src/main/java/com/example/bloodbankmanagementapp/BloodBankManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.bloodbankmanagementapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodBankManagementAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BloodBankManagementAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bloodbankmanagementapp/BloodBankManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bloodbankmanagementapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodBankManagementAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
