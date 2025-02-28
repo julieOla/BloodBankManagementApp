ce38152f7ff729529b55ed7d53058a6fc377159f
JulieOlamijuwon <d00215779@student.dkit.ie>
2025-02-28 15:46:23 +0000

    Fouth commit: Updated bbmStyle.css, registrationForm.html, DonorForm , UserController,User , login)

--- src/main/java/BloodBankManagementApp/business/User.java
+++ src/main/java/BloodBankManagementApp/business/User.java
@@ -25,7 +25,7 @@ public class User {
 
     private String email;
 
-
+    //@Enumerated(EnumType.STRING)
     private Role role;
 
     public enum Role {
--- src/main/java/BloodBankManagementApp/controller/UserController.java
+++ src/main/java/BloodBankManagementApp/controller/UserController.java
@@ -5,8 +5,10 @@ import BloodBankManagementApp.persistence.UserDao;
 import BloodBankManagementApp.persistence.UserDaoImpl;
 import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Role;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -27,8 +29,10 @@ public class UserController {
             @RequestParam(name="email") String email,
             @RequestParam(name="password") String password,
             @RequestParam(name="password2") String password2,
-            @RequestParam(name="role") User.Role role,
-            //@RequestParam(name="address") String address,
+            @RequestParam(name="role") String role,
+            //Role userrole = Enum.EnumDesc<E>,
+            //assignRole(@RequestParam("role") User.Role role),
+    //@RequestParam(name="address") String address,
             //@RequestParam(name="dateOfBirth") String dateOfBirth,
             //Model model, HttpSession session) throws InvalidKeySpecException, NoSuchAlgorithmException {
             Model model,
@@ -39,14 +43,16 @@ public class UserController {
 
 
         // username validation
-
+       String errorMsg = null;
         if (username.isBlank()){
-            String message = "Username was left blank";
-            model.addAttribute("message", message);
+            errorMsg = "Username was left blank";
+            /*model.addAttribute("message", errorMsg);
             System.out.println("Username was left blank");
             //return "user_indexSignUp";
             //return  "registration";
             return  "registrationForm";
+
+             */
         }
 
         Pattern usernameRegex = Pattern.compile("^[a-zA-Z]{3,25}$");
@@ -54,32 +60,37 @@ public class UserController {
         boolean matchfoundUsername = match.find();
 
         if (!matchfoundUsername){
-            String message = "Username must be between 3-25 characters, letters only";
-            model.addAttribute("message", message);
+            errorMsg = "Username must be between 3-25 characters, letters only";
+           /* model.addAttribute("message", errorMsg);
             System.out.println("Username must be between 3-25 characters, letters only");
 
             return  "registrationForm";
+
+            */
         }
 
         // email validation
 
         if(email.isBlank()){
-            String message3 = "Email was left blank";
-            model.addAttribute("message3", message3);
+            errorMsg = "Email was left blank";
+            /*model.addAttribute("message", errorMsg);
             System.out.println("Email was left blank");
 
             return  "registrationForm";
+
+             */
         }
 
         // password validation
 
         if (password.isBlank()){
-            String message4 = "Password was left blank";
-            model.addAttribute("message4", message4);
+            errorMsg = "Password was left blank";
+            /*model.addAttribute("message", errorMsg);
             System.out.println("Password was left blank");
-            //return "user_indexSignUp";
-            //return  "registration";
+
             return  "registrationForm";
+
+             */
         }
 
         Pattern passwordRegex = Pattern.compile("^(?=.[a-z])(?=.[A-Z])(?=.\\d)(?=.[@$!%?&])[A-Za-z\\d@$!%?&]{7,70}$");
@@ -87,47 +98,76 @@ public class UserController {
         boolean matchfoundPassword = match1.find();
 
         if (!matchfoundPassword){
-            String message4 = "Password didnt have at least 7-70 characters, one uppercase letter, one lowercase letter, one number and one special character";
-            model.addAttribute("message4", message4);
-            System.out.println("Password must have at least 7 characters and maximum 70 characters, one uppercase letter, one lowercase letter and one number");
+            errorMsg = "Password didnt have at least 7-70 characters, one uppercase letter, one lowercase letter, one number and one special character";
+            /*model.addAttribute("message", message);
+            //System.out.println("Password must have at least 7 characters and maximum 70 characters, one uppercase letter, one lowercase letter and one number");
 
             return  "registrationForm";
+
+             */
         }
 
         if (password2.isBlank()){
-            String message5 = "Confirm Password was left blank";
+            errorMsg = "Confirm Password was left blank";
+            /*String message5 = "Confirm Password was left blank";
             model.addAttribute("message5", message5);
             System.out.println("Confirm password was left blank");
-
             return  "registrationForm";
+
+             */
         }
 
         if (!password.equals(password2)){
-            String message5 = "Password and Confirm Password didnt match";
+            errorMsg = "Password and Confirm Password didnt match";
+            /*String message5 = "Password and Confirm Password didnt match";
             model.addAttribute("message5", message5);
             System.out.println("Passwords dont match");
 
-            return  "registrationForm";
+            return  "registrationForm";*/
         }
+       if (errorMsg != null) {
+           model.addAttribute("errorMessage", errorMsg);
+           //return  "registerUser";
+           //return  "register";
+           return "registrationForm";
+       }
         String view = "";
 
+        User.Role newUserRole;
+        switch (role){
+            case "admin":
+                newUserRole = User.Role.ADMIN;
+                break;
+            case "donor":
+                newUserRole = User.Role.DONOR;
+                break;
+            case "employee":
+                newUserRole = User.Role.EMPLOYEE;
+                break;
+            case "hospital_Admin":
+                newUserRole = User.Role.HOSPITAL_ADMIN;
+                break;
 
+            default:
+                newUserRole= User.Role.DONOR;
+        }
         // Build new user with the detail entered in registration form
         User newUser = User.builder()
                 .userID(0)
                 .username(username)
                 .password(password)
                 .email(email)
-                .role(User.Role.DONOR)
+                .role(newUserRole)
                 .build();
 
         //String view = "";
         UserDao userDao = new UserDaoImpl("database.properties");
         // Convert the role string to the Role enum
         //User.Role userRole = User.Role.valueOf(role.toUpperCase());
-        User.Role userRole = User.Role.DONOR;
+        //User.Role userRole = User.Role.DONOR;
         //User u = new User(username, password,email,userRole.name().toUpperCase());
-        User u = new User(0,username, password,email,userRole);
+        //User u = new User(0,username, password,email,userRole);
+       User u = new User(0,username, password,email,newUserRole);
         boolean added = userDao.registerUser(u);
         if(added ){
             view = "registerSuccess";
